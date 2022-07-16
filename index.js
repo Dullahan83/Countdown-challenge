9bf6ba1811303b1d2a24b1df66ca9efef7008fba
Renaud <maladrix@hotmail.com>
2022-07-16 15:29:57 +0200

    push project

---
+++ index.js
@@ -0,0 +1,77 @@
+const input = document.getElementById("chosenDate")
+input.addEventListener("change", (e) =>{
+    let inputValue = e.target.value
+    localStorage.setItem("chosenDate", inputValue)
+})
+
+
+
+const chosenDay = new Date(localStorage.getItem("chosenDate"));
+
+
+function countdown(){
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    let now = new Date();
+    let timeDiff = chosenDay - now;
+
+    const seconds = Math.floor((timeDiff % minute) / second)
+    const minutes = Math.floor(timeDiff % hour / minute)
+    const hours = Math.floor(timeDiff % day / hour)
+    const days = Math.floor(timeDiff / day)
+
+    if(timeDiff > 0){
+        if(seconds < 10){
+            document.querySelector(".seconds .ten").textContent = "0"
+            document.querySelector(".seconds .one").textContent = seconds.toString()[0]
+        }
+        else{
+            document.querySelector(".seconds .ten").textContent = seconds.toString()[0]
+            document.querySelector(".seconds .one").textContent = seconds.toString()[1]
+        }
+    
+        if(minutes < 10){
+            document.querySelector(".minutes .ten").textContent = "0"
+            document.querySelector(".minutes .one").textContent = minutes.toString()[0]
+        }
+        else{
+            document.querySelector(".minutes .ten").textContent = minutes.toString()[0]
+            document.querySelector(".minutes .one").textContent = minutes.toString()[1]
+        }
+    
+        if(hours < 10){
+            document.querySelector(".hours .ten").textContent = "0"
+            document.querySelector(".hours .one").textContent = hours.toString()[0]
+        }
+        else{
+            document.querySelector(".hours .ten").textContent = hours.toString()[0]
+            document.querySelector(".hours .one").textContent = hours.toString()[1]
+        }
+    
+        if(days < 10){
+            document.querySelector(".days .ten").textContent = "0"
+            document.querySelector(".days .one").textContent = days.toString()[0]
+        }
+        else{
+            document.querySelector(".days .ten").textContent = days.toString()[0]
+            document.querySelector(".days .one").textContent = days.toString()[1]
+        }
+    }
+    else{
+        const cells = document.querySelectorAll(".cell p")
+        cells.forEach(cell => {
+            cell.textContent = 0
+        });
+    }
+}
+countdown()
+
+
+
+const button = document.querySelector("#bonus button")
+button.addEventListener("click", () => {
+    const interval = setInterval(countdown, 1000)
+})
